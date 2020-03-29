508c822126ded6187921ddf5a31fd489acfe95f9
Saulo Braga <saulo.braga@live.com>
2020-03-29 19:36:36 -0300

    Finish of web frontend develop

--- web/src/pages/NewIncident/index.js
+++ web/src/pages/NewIncident/index.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import Api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 import './styles.css';
 
 export default function NewIncident(){
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+    const ongId = localStorage.getItem('ongId');
+    const history = useHistory();
+
+    async function handleNewIncident(event){
+        
+        event.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value
+        };
+
+        try {
+            await Api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId
+                }
+            });
+
+            history.push('/profile');
+        } catch(err){
+            
+        }
+    }
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -15,10 +45,10 @@ export default function NewIncident(){
                     <p>Descreva o caso detalhadamente para encontrar um heroi para resolver isso.</p>
                     <Link className="link" to="/profile"><FiArrowLeft size={16} color="#E02041"/>Voltar para home</Link>
                 </section>
-                <form>
-                    <input type="text" placeholder="Titulo do caso" />
-                    <textarea placeholder="Descrição" ></textarea>
-                    <input type="text" placeholder="Valor em reais" />
+                <form onSubmit={handleNewIncident}>
+                    <input type="text" value={title} onChange={ e => setTitle(e.target.value) } placeholder="Titulo do caso" />
+                    <textarea value={description} onChange={ e => setDescription(e.target.value) } placeholder="Descrição" ></textarea>
+                    <input value={value} onChange={ e => setValue(e.target.value) } type="text" placeholder="Valor em reais" />
                     <button type="submit" className="button">Cadastrar</button>
                 </form>
             </div>
--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2} from 'react-icons/fi';
 
 import Api from '../../services/api';
@@ -11,6 +11,7 @@ import logoImg from '../../assets/logo.svg';
 export default function Profile(){
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
+    const history = useHistory();
 
     const [incidents, setIncidents] = useState([]);
 
@@ -23,6 +24,7 @@ export default function Profile(){
             setIncidents(response.data);
         });
     }, [ongId]);
+    
     async function handleDelete(id){
         try{
             await Api.delete(`/incidents/${id}`, {
@@ -35,13 +37,20 @@ export default function Profile(){
 
         }
     }
+
+    function handleLogout(){
+        localStorage.clear();
+
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero"/>
                 <span>Bem vinda, {ongName}</span>
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button"><FiPower size={18} color="#E02041" /></button>
+                <button onClick={handleLogout} type="button"><FiPower size={18} color="#E02041" /></button>
             </header>
             <h1>Casos cadastrados</h1>
             <ul>
